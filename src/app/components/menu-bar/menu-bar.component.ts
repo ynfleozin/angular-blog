5e413ec518e16852faaff9a1f0efbde7a8633f1f
Leonardo Alvarenga <102129665+ynfleozin@users.noreply.github.com>
2024-02-23 14:44:25 -0300

    Update Project

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.css'
+  styleUrls: ['./menu-bar.component.css', './menu-bar.responsive.component.css']
 })
 export class MenuBarComponent {
 
